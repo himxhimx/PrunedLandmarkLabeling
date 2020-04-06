fed7049b7be0f1c397dbe515daa2dfa46fdf0ef8
franklcf104 <frank.lcf104@gmail.com>
2020-04-06 22:12:16 +0800

    add load_index function

--- ppl.py
+++ ppl.py
@@ -65,32 +65,10 @@ class PrunedLandmarkLabeling(object):
         return result
 
     def load_index(self, index_file_path):
-        #G = nx.DiGraph()
-        #f = open(index_file_path, 'r')
-        #data = f.readlines()
-        #f.close()
-        #for idx, lines in enumerate(data):
-        #    if (idx < 1)
-        #        continue
-        result = {
-	        "1": {
-                "backward": [1,0],
-		        "forward": [(1,0)]
-	        },
-	        "2": {
-		        "backward": [(1,21),(2,0)],
-		        "forward": [(1,1),(2,0)]
-	        },
-	        "3": {
-		        "backward": [(1,10),(2,1),(3,0)],
-		        "forward": [(1,3),(2,15),(3.0)]
-	        },
-            "4": {
-                "backward": [(1,8),(2,3),(3,2),(4,0)],
-                "forward": [(1,5),(2,13),(3,2),(4,0)]
-            }
-        }
-        #print(result)
+        f = open(index_file_path, 'r')
+        data = f.read()
+        result = json.loads(data)
+        f.close()
         return result
 
     def gen_test_order(self):
